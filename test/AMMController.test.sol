62a7e6b014df40a6753d602ab601ad172053647e
Jay <lelerukjaymoh@gmail.com>
2023-07-16 15:27:24 +0300

    refactor: remove unused code

--- test/AMMController.test.sol
+++ test/AMMController.test.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "forge-std/Test.sol";
 import {AMMv2Controller, IAMMv2Controller} from "@dera/controllers/AMMv2Controller.sol";
 import {Treasury} from "@dera/Treasury.sol";
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
